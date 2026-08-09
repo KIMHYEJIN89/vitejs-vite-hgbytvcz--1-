eed639f023ec6691780f17371a305fd575c1fbd3
KIMHYEJIN <chch2222@gmail.com>
2026-08-09 20:36:08 +0900

    useProduct commit

--- src/hooks/useProductDetail.js
+++ src/hooks/useProductDetail.js
@@ -1,14 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 import api from '../utils/api';
-const fetchProductDetail = (productId) => {
-  return api.get(`/products/${productId}`);
+
+const fetchProductDetail = async (productId) => {
+  const res = await api.get('/'); // 전체 데이터 가져오기
+  const product = res.data.products.find((p) => p.id === Number(productId));
+
+  if (!product) {
+    throw new Error('상품을 찾을 수 없습니다');
+  }
+
+  return product;
 };
 
-export const useProductDetail = (produtId) => {
+export const useProductDetail = (productId) => {
   return useQuery({
-    queryKey: ['product_detail', produtId],
-    queryFn: () => fetchProductDetail(produtId),
+    queryKey: ['product_detail', productId],
+    queryFn: () => fetchProductDetail(productId),
     suspense: true,
-    select: (res) => res.data,
   });
 };
